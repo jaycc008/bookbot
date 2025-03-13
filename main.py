a2b414c660f0e5d91c35410a944b645c8f16f3bc
jayce008 <jayce.ardon@gmail.com>
2025-03-13 16:07:31 +0100

    refactor

--- main.py
+++ main.py
@@ -1,47 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    words_count = count_words(text)
-    chars_dict = count_chars(text)
-    dict_list = create_dict_list(chars_dict)
-    dict_list.sort(reverse=True, key=sort_on)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{words_count} words found in the document\n")
-
-    for dict in dict_list:
-        print(f"The '{dict['name']}' character was found {dict['num']} times")
-    print("--- End report ---")
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text):
-    words = text.split()
-    return len(words)
 
-def count_chars(text):
-    chars_dict = {}
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-    for char in text.lower():
-        if char.isalpha():
-            if char in chars_dict:
-                chars_dict[char] += 1
-            else:
-                chars_dict[char] = 1
+    print("============= END ===============")
 
-    return(chars_dict)
-
-def create_dict_list(dict):
-    dict_list = []
-    for key, value in dict.items():
-        dict_list.append({"name": key, "num": value})
-
-    return dict_list
-
-def sort_on(dict):
-    return dict["num"]
 
 main()
